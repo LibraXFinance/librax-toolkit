0df1263279e44129c79f640decf8f35a80fb155f
Lib <joji.sekiya@gmail.com>
2022-05-14 23:43:05 +0900

    change input color

--- packages/librax-uikit/src/theme/colors.ts
+++ packages/librax-uikit/src/theme/colors.ts
@@ -61,7 +61,7 @@ export const darkColors: Colors = {
   dropdown: "#1E1D20",
   dropdownDeep: "#100C18",
   invertedContrast: "#191326",
-  input: "#372F47",
+  input: "#CCCCCC",
   inputSecondary: "#262130",
   primaryDark: "#0098A1",
   tertiary: "#353547",
