7b97eb3de8daad6951ecba1ca5db8126c82aeb4f
Lib <joji.sekiya@gmail.com>
2022-08-03 15:31:53 +0900

    Switching information onto Avalanche

--- packages/librax-profile-sdk/src/constants/__mocks__/nfts.ts
+++ packages/librax-profile-sdk/src/constants/__mocks__/nfts.ts
@@ -7,15 +7,15 @@ import { IPFS_GATEWAY } from "../common";
 export const nftSources: NftSource = {
   [NftType.PANCAKE]: {
     address: {
-      56: "0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07",
-      97: "0x60935F36e4631F73f0f407e68642144e07aC7f5E",
+      43114: "0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07",
+      43113: "0x60935F36e4631F73f0f407e68642144e07aC7f5E",
     },
     identifierKey: "image",
   },
   [NftType.MIXIE]: {
     address: {
-      56: "0xa251b5EAa9E67F2Bc8b33F33e20E91552Bf85566",
-      97: "",
+      43114: "0xa251b5EAa9E67F2Bc8b33F33e20E91552Bf85566",
+      43113: "",
     },
     identifierKey: "otherIdentifier",
   },
